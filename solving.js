59c20d58090ed5bcd3b313ee0fb2b8239a5b430f
Rupangkar majumder <145119687+Rupangkar@users.noreply.github.com>
2023-11-05 08:41:00 +0600

    y

---
+++ solving.js
@@ -0,0 +1,97 @@
+/* 
+ const numbers =[12,23,43,54,55,456];
+
+function smallestNunber(arrayDatas) {
+  let min =arrayDatas[0]
+
+  for( i = 0; i < arrayDatas.length; i++) {
+    const element =arrayDatas[i];
+    if(element < min){
+      min = element;
+    }
+}
+return min
+
+  }
+
+  let result = smallestNunber(numbers)
+  console.log(result); */
+   
+//   const player =['Massi','Dimariy','Dimariy','Devala','hegoin','Neymer','Tavej','Neymer'];
+
+//   function removeDiployment(names) {
+//     let unique = [];
+//     for( i = 0; i < names.length; i++) {
+//       const element = names [i];
+//       if(unique.indexOf(element) == -1) {
+//         unique.push(element);
+//       }
+//     }
+//     return unique;
+//   }
+//  let result =removeDiployment(player);
+//  console.log(result);
+
+
+// for of loop 
+
+
+// const player =['Rifat','kamal','kamal','Sajon','dipu','dipu'];
+// function removeDiployment(names) {
+//   let unique = [];
+
+//   for(const item of names) {
+//     if(unique.indexOf(item) == -1)
+//     unique.push(item);
+
+//   }
+//   return unique;
+// }
+//  let result =removeDiployment(player)
+// console.log(result);
+
+//reverse
+  
+// function addNumber(number1,number2){
+//   const total = number1 + number2;
+//   return total;
+// }
+// let result = addNumber(23,34);
+// console.log(result);
+
+
+// for (i = 1; i<= 100; i++){
+
+//  if(i % 3 == 0 && i % 5 == 0) {
+//     console.log('foo bar');
+//   }
+//   else if(i % 3 == 0){
+//     console.log("foo");
+//   }
+//   else if(i % 5 == 0){
+//     console.log("bar");
+//   }
+ 
+//   else {
+//     console.log(i);
+  
+//   }
+  
+// }
+
+
+// function calculation(int1,int2){
+//   const initial = 100;
+//   const multiply = int1 * int2;
+
+//   if(initial < multiply) {
+//     console.log(true);
+//   }
+//   else if(initial <= multiply){
+//     console.log(false);
+//   }else{
+//     console.log("sumthing is rong");
+//   }
+// }
+// let result =calculation (10,20);
+// console.log(result);
